f4ee4a3f2a3978077ba2bb7b6501b9d47c545786
thobui14ig <buithanhtho14ig@gmail.com>
2025-04-30 10:54:28 +0700

    [fear] add facker

--- src/modules/facebook/facebook.service.ts
+++ src/modules/facebook/facebook.service.ts
@@ -210,30 +210,37 @@ export class FacebookService {
   }
 
   async getCommentByToken(postId: string, proxy: ProxyEntity, token: TokenEntity) {
-    const acceptLanguages = [
-      "vi-VN,vi;q=0.9,en-US;q=0.8",
-      "en-US,en;q=0.9",
-      "fr-FR,fr;q=0.9,en;q=0.8"
-    ];
-    const userAgent = faker.internet.userAgent()
-    const apceptLanguage = acceptLanguages[Math.floor(Math.random() * acceptLanguages.length)]
     const httpsAgent = this.getHttpAgent(proxy)
     console.log("🚀 ~ getCommentByToken ~ postId:", postId)
     try {
+      const userAgents = [
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64)...',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)...',
+        'Mozilla/5.0 (Linux; Android 10; SM-G975F)...'
+      ];
+
+      const languages = [
+        'en-US,en;q=0.9',
+        'vi-VN,vi;q=0.9,en-US;q=0.8,en;q=0.7',
+        'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7'
+      ];
+
+      const platforms = ['"Windows"', '"macOS"', '"Linux"'];
+
       const headers = {
         'authority': 'graph.facebook.com',
         'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="99", "Opera";v="85"',
         'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
+        'sec-ch-ua-platform': platforms[Math.floor(Math.random() * platforms.length)],
         'upgrade-insecure-requests': '1',
-        'user-agent': userAgent,
+        'user-agent': userAgents[Math.floor(Math.random() * userAgents.length)],
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
         'sec-fetch-site': 'none',
         'sec-fetch-mode': 'navigate',
         'sec-fetch-user': '?1',
         'sec-fetch-dest': 'document',
-        'accept-language': apceptLanguage,
-      }
+        'accept-language': languages[Math.floor(Math.random() * languages.length)],
+      };
 
       const params = {
         "order": "reverse_chronological",
@@ -241,6 +248,7 @@ export class FacebookService {
         "access_token": token.tokenValue,
         "created_time": "created_time"
       }
+
 
       const dataCommentToken = await firstValueFrom(
         this.httpService.get(`https://graph.facebook.com/${postId}/comments`, {
