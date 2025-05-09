2604b313d5b703b8ad077566fefd21a1b7e01c3e
thobui14ig <buithanhtho14ig@gmail.com>
2025-05-09 20:54:04 +0700

    [feat] comment count

--- src/modules/facebook/facebook.service.ts
+++ src/modules/facebook/facebook.service.ts
@@ -608,6 +608,7 @@ export class FacebookService {
 
   async getProfileLink(url: string, proxy: ProxyEntity) {
     const token = await this.getTokenActiveFromDb()
+    const cookieEntity = await this.getCookieActiveFromDb()
 
     try {
       console.log("🚀 ~ MonitoringService ~ cronjobHandleProfileUrl ~ this.isHandleUrl:", url)
@@ -647,7 +648,6 @@ export class FacebookService {
         "method": "GET"
       });
       const htmlContent = await response.text()
-      // writeFile(htmlContent, '2')
       // const matches = [...htmlContent.matchAll(/href="([^"]+)"/g)];
       // const expectedUrl = matches[1] ? matches[1][1].replace('amp;', '') : null;
       // console.log("🚀 ~ getProfileLink ~ expectedUrl:", expectedUrl)
@@ -696,40 +696,87 @@ export class FacebookService {
       }
 
       //case 2: cần token
-      if (!token) {
+      if (token) {
+        const params = {
+          "order": "reverse_chronological",
+          "limit": "1000",
+          "access_token": token.tokenValue,
+          "created_time": "created_time"
+        }
+
+        const responseV1 = await firstValueFrom(
+          this.httpService.get(url, {
+            headers: { ...headers },
+            httpsAgent,
+            params
+          }),
+        );
+        const htmlContentV1 = responseV1.data
+
+        const match1 = htmlContentV1.match(/"video_id":"(.*?)"/);
+
+        if (match1 && match1[1]) {
+          const postId = match1[1]
+          console.log("🚀 ~ getProfileLink ~ match1[1]:", postId)
+
+          return {
+            type: LinkType.PRIVATE,
+            name: url,
+            postId: postId,
+          }
+        }
+      }
+
+      if (cookieEntity) {
+        {
+          const newCookies = this.changeCookiesFb(cookieEntity.cookie);
+
+          const responseWithCookie = await fetch(url, {
+            "headers": {
+              "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+              "accept-language": "en-US,en;q=0.9,vi;q=0.8",
+              "cache-control": "max-age=0",
+              "dpr": "1",
+              "priority": "u=0, i",
+              "sec-ch-prefers-color-scheme": "light",
+              "sec-ch-ua": "\"Google Chrome\";v=\"135\", \"Not-A.Brand\";v=\"8\", \"Chromium\";v=\"135\"",
+              "sec-ch-ua-full-version-list": "\"Google Chrome\";v=\"135.0.7049.116\", \"Not-A.Brand\";v=\"8.0.0.0\", \"Chromium\";v=\"135.0.7049.116\"",
+              "sec-ch-ua-mobile": "?0",
+              "sec-ch-ua-model": "\"\"",
+              "sec-ch-ua-platform": "\"Windows\"",
+              "sec-ch-ua-platform-version": "\"10.0.0\"",
+              "sec-fetch-dest": "document",
+              "sec-fetch-mode": "navigate",
+              "sec-fetch-site": "same-origin",
+              "sec-fetch-user": "?1",
+              "upgrade-insecure-requests": "1",
+              "viewport-width": "856",
+              "cookie": this.formatCookies(newCookies)
+            },
+            "referrerPolicy": "strict-origin-when-cross-origin",
+            "body": null,
+            "method": "GET"
+          });
+          const matchpublic = (await responseWithCookie.text() as string).match(/"post_id":"(.*?)"/);
+
+          if (matchpublic && matchpublic[1]) {
+            const postId = matchpublic[1]
+            if (postId) {
+              return {
+                type: LinkType.PRIVATE,
+                name: url,
+                postId: postId,
+              }
+            }
+          }
+        }
+      }
+
+      if (!token || !cookieEntity) {
         return {
           type: LinkType.UNDEFINED,
         }
       }
-      const params = {
-        "order": "reverse_chronological",
-        "limit": "1000",
-        "access_token": token.tokenValue,
-        "created_time": "created_time"
-      }
-
-      const responseV1 = await firstValueFrom(
-        this.httpService.get(url, {
-          headers: { ...headers },
-          httpsAgent,
-          params
-        }),
-      );
-      const htmlContentV1 = responseV1.data
-
-      const match1 = htmlContentV1.match(/"video_id":"(.*?)"/);
-
-      if (match1 && match1[1]) {
-        const postId = match1[1]
-        console.log("🚀 ~ getProfileLink ~ match1[1]:", postId)
-
-        return {
-          type: LinkType.PRIVATE,
-          name: url,
-          postId: postId,
-        }
-      }
-
 
       return {
         type: LinkType.DIE,
