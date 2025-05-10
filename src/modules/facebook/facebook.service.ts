1c30743609137b141dcc6b1733cf45a05f2b2654
thobui14ig <buithanhtho14ig@gmail.com>
2025-05-10 22:16:11 +0700

    [feat] comment count

--- src/modules/facebook/facebook.service.ts
+++ src/modules/facebook/facebook.service.ts
@@ -625,32 +625,32 @@ export class FacebookService {
       console.log("----------Đang lấy thông tin url:", url)
       const { headers, cookies } = getHeaderProfileLink()
 
-      const response = await fetch(url, {
-        "headers": {
-          "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-          "accept-language": "en-US,en;q=0.9,vi;q=0.8",
-          "cache-control": "max-age=0",
-          "dpr": "1",
-          "priority": "u=0, i",
-          "sec-ch-prefers-color-scheme": "light",
-          "sec-ch-ua": "\"Google Chrome\";v=\"135\", \"Not-A.Brand\";v=\"8\", \"Chromium\";v=\"135\"",
-          "sec-ch-ua-full-version-list": "\"Google Chrome\";v=\"135.0.7049.116\", \"Not-A.Brand\";v=\"8.0.0.0\", \"Chromium\";v=\"135.0.7049.116\"",
-          "sec-ch-ua-mobile": "?0",
-          "sec-ch-ua-model": "\"\"",
-          "sec-ch-ua-platform": "\"Windows\"",
-          "sec-ch-ua-platform-version": "\"10.0.0\"",
-          "sec-fetch-dest": "document",
-          "sec-fetch-mode": "navigate",
-          "sec-fetch-site": "same-origin",
-          "sec-fetch-user": "?1",
-          "upgrade-insecure-requests": "1",
-          "viewport-width": "856"
-        },
-        "referrerPolicy": "origin-when-cross-origin",
-        "body": null,
-        "method": "GET"
-      });
-      const htmlContent = await response.text()
+      const response = await await firstValueFrom(
+        this.httpService.get(url, {
+          headers: {
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "accept-language": "en-US,en;q=0.9,vi;q=0.8",
+            "cache-control": "max-age=0",
+            "dpr": "1",
+            "priority": "u=0, i",
+            "sec-ch-prefers-color-scheme": "light",
+            "sec-ch-ua": "\"Google Chrome\";v=\"135\", \"Not-A.Brand\";v=\"8\", \"Chromium\";v=\"135\"",
+            "sec-ch-ua-full-version-list": "\"Google Chrome\";v=\"135.0.7049.116\", \"Not-A.Brand\";v=\"8.0.0.0\", \"Chromium\";v=\"135.0.7049.116\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-model": "\"\"",
+            "sec-ch-ua-platform": "\"Windows\"",
+            "sec-ch-ua-platform-version": "\"10.0.0\"",
+            "sec-fetch-dest": "document",
+            "sec-fetch-mode": "navigate",
+            "sec-fetch-site": "same-origin",
+            "sec-fetch-user": "?1",
+            "upgrade-insecure-requests": "1",
+            "viewport-width": "856"
+          },
+          httpsAgent,
+        }),
+      );
+      const htmlContent = response.data
       const matchVideoPublic = htmlContent.match(/,"actors":(\[.*?\])/);
 
       //case 1: video, post public
