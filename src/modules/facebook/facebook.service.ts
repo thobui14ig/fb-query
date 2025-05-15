de32a85594753f44da2e2b8e184eb0e73d7ad58d
thobui14ig <buithanhtho14ig@gmail.com>
2025-05-15 20:14:07 +0700

    ok

--- src/modules/facebook/facebook.service.ts
+++ src/modules/facebook/facebook.service.ts
@@ -847,48 +847,46 @@ export class FacebookService {
       }
 
       if (cookieEntity) {
-        {
-          const newCookies = this.changeCookiesFb(cookieEntity.cookie);
+        const newCookies = this.changeCookiesFb(cookieEntity.cookie);
 
-          const responseWithCookie = await firstValueFrom(
-            this.httpService.get(url, {
-              "headers": {
-                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-                "accept-language": "en-US,en;q=0.9,vi;q=0.8",
-                "cache-control": "max-age=0",
-                "dpr": "1",
-                "priority": "u=0, i",
-                "sec-ch-prefers-color-scheme": "light",
-                "sec-ch-ua": "\"Google Chrome\";v=\"135\", \"Not-A.Brand\";v=\"8\", \"Chromium\";v=\"135\"",
-                "sec-ch-ua-full-version-list": "\"Google Chrome\";v=\"135.0.7049.116\", \"Not-A.Brand\";v=\"8.0.0.0\", \"Chromium\";v=\"135.0.7049.116\"",
-                "sec-ch-ua-mobile": "?0",
-                "sec-ch-ua-model": "\"\"",
-                "sec-ch-ua-platform": "\"Windows\"",
-                "sec-ch-ua-platform-version": "\"10.0.0\"",
-                "sec-fetch-dest": "document",
-                "sec-fetch-mode": "navigate",
-                "sec-fetch-site": "same-origin",
-                "sec-fetch-user": "?1",
-                "upgrade-insecure-requests": "1",
-                "viewport-width": "856",
-                "cookie": this.formatCookies(newCookies)
-              },
-              httpsAgent,
-            }),
-          );
+        const responseWithCookie = await firstValueFrom(
+          this.httpService.get(url, {
+            "headers": {
+              "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+              "accept-language": "en-US,en;q=0.9,vi;q=0.8",
+              "cache-control": "max-age=0",
+              "dpr": "1",
+              "priority": "u=0, i",
+              "sec-ch-prefers-color-scheme": "light",
+              "sec-ch-ua": "\"Google Chrome\";v=\"135\", \"Not-A.Brand\";v=\"8\", \"Chromium\";v=\"135\"",
+              "sec-ch-ua-full-version-list": "\"Google Chrome\";v=\"135.0.7049.116\", \"Not-A.Brand\";v=\"8.0.0.0\", \"Chromium\";v=\"135.0.7049.116\"",
+              "sec-ch-ua-mobile": "?0",
+              "sec-ch-ua-model": "\"\"",
+              "sec-ch-ua-platform": "\"Windows\"",
+              "sec-ch-ua-platform-version": "\"10.0.0\"",
+              "sec-fetch-dest": "document",
+              "sec-fetch-mode": "navigate",
+              "sec-fetch-site": "same-origin",
+              "sec-fetch-user": "?1",
+              "upgrade-insecure-requests": "1",
+              "viewport-width": "856",
+              "cookie": this.formatCookies(newCookies)
+            },
+            httpsAgent,
+          }),
+        );
 
-          const text = responseWithCookie.data
-          const matchpublic = text.match(/"post_id":"(.*?)"/);
+        const text = responseWithCookie.data
+        const matchpublic = text.match(/"post_id":"(.*?)"/);
 
-          if (matchpublic && matchpublic[1]) {
-            const postId = matchpublic[1]
-            console.log("🚀 ~ getProfileLink ~ postId:", postId)
-            if (postId) {
-              return {
-                type: LinkType.PRIVATE,
-                name: url,
-                postId: postId,
-              }
+        if (matchpublic && matchpublic[1]) {
+          const postId = matchpublic[1]
+          console.log("🚀 ~ getProfileLink ~ postId:", postId)
+          if (postId) {
+            return {
+              type: LinkType.PRIVATE,
+              name: url,
+              postId: postId,
             }
           }
         }
