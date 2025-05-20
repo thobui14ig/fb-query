dc8d2e3592f755ab635f45b2ab70b369d843172a
thobui14ig <buithanhtho14ig@gmail.com>
2025-05-20 22:46:47 +0700

    ok

--- src/modules/links/links.module.ts
+++ src/modules/links/links.module.ts
@@ -3,11 +3,12 @@ import { LinkService } from './links.service';
 import { LinkEntity } from './entities/links.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LinkController } from './links.controller';
+import { DelayEntity } from '../setting/entities/delay.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([LinkEntity])],
+  imports: [TypeOrmModule.forFeature([LinkEntity, DelayEntity])],
   controllers: [LinkController],
   providers: [LinkService],
   exports: [LinkService],
 })
-export class LinkModule {}
+export class LinkModule { }
