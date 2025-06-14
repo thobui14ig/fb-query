272ef4798e730d9a9c7a36f8ed23f46d02478237
thobui14ig <buithanhtho14ig@gmail.com>
2025-06-14 12:34:27 +0700

    [ok]

--- src/modules/facebook/usecase/get-comment-private/get-comment-private.i.ts
+++
@@ -1,6 +0,0 @@
-import { ICommentResponse } from "../../facebook.service.i"
-
-export interface IGetCmtPrivateResponse {
-    hasData: boolean
-    data?: ICommentResponse | null
-}
--- src/modules/facebook/usecase/get-comment-public/get-comment-public.i.ts
+++
@@ -1,6 +0,0 @@
-import { ICommentResponse } from "../../facebook.service.i"
-
-export interface IGetCmtPublicResponse {
-    hasData: boolean
-    data?: ICommentResponse
-}
